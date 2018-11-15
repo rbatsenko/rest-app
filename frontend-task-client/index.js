5ea80e02728f999921b5e52df4a736e0ebc0cd6e
rbatsenko <rbatsenko@gmail.com>
2018-11-15 15:40:49 +0100

    normalize css, improve logic

--- frontend-task-client/index.js
+++ frontend-task-client/index.js
@@ -1,33 +1,54 @@
 import "babel-polyfill";
-import './styles/styles.scss';
 import format from 'xml-formatter';
 import hljs from 'highlight.js/lib/highlight';
 import json from 'highlight.js/lib/languages/json';
 import xml from 'highlight.js/lib/languages/xml';
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('xml', xml);
-import 'highlight.js/styles/github.css'
+import 'normalize.css';
+import 'highlight.js/styles/github.css';
+import './styles/styles.scss';
 
-const responseContainer = document.getElementById('response');
+let responseSelect = document.getElementById('response-type');
+let requestDataFormatContainer = document.getElementsByClassName('request__url__format')[0];
+let responseContainer = document.getElementById('response');
+let fetchBtn = document.getElementById('fetch-data');
+let forceFetch = document.getElementById('force-fetch');
+
+fetchBtn.addEventListener('click', () => {
+  let responseFormat = responseSelect.value;
+
+  responseContainer.classList.remove(responseContainer.classList[1]);
+  responseContainer.classList.add(responseFormat);
+  requestDataFormatContainer.innerHTML = responseFormat;
+  forceFetch.checked === true ? fetchBtn.disabled = false : fetchBtn.disabled = true;
+  getAdvertisers(responseFormat);
+});
+
+responseSelect.addEventListener('change', () => {
+  let responseFormat = responseSelect.value;
+  requestDataFormatContainer.textContent.length !== 0 && requestDataFormatContainer.textContent === responseFormat ? fetchBtn.disabled = true : fetchBtn.disabled = false;
+});
+
+forceFetch.addEventListener('change', function() {
+  this.checked === true ? fetchBtn.disabled = false : fetchBtn.disabled = true;
+});
 
 async function getAdvertisers(type) {
-  if (type === 'xml') {
-    try {
-      const response = await fetch('http://localhost:3000/advertisers/?format=' + type).then(r => r.text());
-      responseContainer.textContent = format(response);
-      hljs.initHighlighting();
-    } catch(error) {
-      console.log(error);
-    }
-  } else if (type === 'json') {
-    try {
-      const response = await fetch('http://localhost:3000/advertisers/?format=' + type).then(r => r.json());
-      responseContainer.textContent = JSON.stringify(response, null, 4);
-      hljs.initHighlighting();
-    } catch(error) {
-      console.log(error);
-    }
+  responseContainer.textContent = 'Fetching data...';
+  try {
+    const response = await fetch('http://localhost:3000/advertisers/?format=' + type)
+      .then(r => { 
+        if (type === 'json') { 
+          return r.json() 
+        } else { 
+          return r.text() 
+        } 
+      });
+
+    type === 'json' ? responseContainer.textContent = JSON.stringify(response, null, 4) : responseContainer.textContent = format(response); // different parsing for different formats
+    hljs.highlightBlock(document.getElementsByTagName('code')[0]);
+  } catch(error) {
+    responseContainer.innerHTML = '<span class="error">' + error + '</span>';
   }
 }
-
-getAdvertisers('xml');
